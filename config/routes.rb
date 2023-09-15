a9e15de2fa48d490589e0f314a2124f508aab361
Matteo Peguri <peguri.1958438@studenti.uniroma1.it>
2023-09-15 11:21:43 +0200

    added devise gem for user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
   get "/authors", to: "authors#index"
   get "/authors/:id", to: "authors#show"
 end
