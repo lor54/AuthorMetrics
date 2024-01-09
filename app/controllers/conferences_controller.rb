94cb0f23e281d9f52c32b58a7dbcf9d2e5d78c7d
manoftardis <filipposerafini2000@gmail.com>
2024-01-09 19:34:20 +0100

    fully implemented pagination for both publications and authors of the conference

--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -44,14 +44,10 @@ class ConferencesController < ApplicationController
   end
 
   def show
-
-    @types = [ 'all', 'article', 'inproceedings', 'proceedings', 'book', 'incollection' ]
+    @years = ['all']
     conferenceId = params[:id] || 0
     title = params[:title] || ''
-    @type = params[:type] || 'all'
-    if !@types.include? (@type)
-      @type = 'all'
-    end
+    @year = params[:year] || 'all'
 
     #we extract the information from the database
     conference = Conference.getConferenceDatabase(conferenceId)
@@ -60,7 +56,7 @@ class ConferencesController < ApplicationController
     @conference['name'] = conference.name
 
     authorsList = Array.new()
-    years = Array.new()
+
 
     conference.publications.each do |publication|
       publication.authors.each do |author|
@@ -68,18 +64,17 @@ class ConferencesController < ApplicationController
           authorsList.push(author.name)
         end
       end
-      if !(years.include?(publication.year))
-        years.push(publication.year)
+      if !(@years.include?(publication.year))
+        @years.push(publication.year)
       end
     end
-    @conference['years'] = years
+    @conference['years'] = @years
     @conference['authors'] = authorsList.paginate(page: params[:author_page], per_page: 20)
-
-    if @type = 'all'
+    if @year == 'all'
       if title != ''
         @conference['publications'] = conference.publications
         .where("publications.title Like ?", "%#{title}%")
-        .paginate(page: params[:page], per_page: 10)
+        .paginate(page: params[:publ_page], per_page: 10)
       else
         @conference['publications'] = conference.publications.paginate(page: params[:publ_page], per_page: 10)
       end
@@ -87,14 +82,13 @@ class ConferencesController < ApplicationController
       if title != ''
         @conference['publications'] = conference.publications
         .where("publications.title Like ?", "%#{title}%")
-        .where(publications: {article_type: @type})
-        .paginate(page: params[:page], per_page: 10)
+        .where(publications: {year: @year})
+        .paginate(page: params[:publ_page], per_page: 10)
       else
         @conference['publications'] = conference.publications
-        .where(publications: {article_type: @type})
-        .paginate(page: params[:page], per_page: 10)
+        .where(publications: {year: @year})
+        .paginate(page: params[:publ_page], per_page: 10)
       end
     end
-    p @conference['publications']
   end
 end
